dab18d6b6197e1c22d059a68958af048da293908
Oscar Lagatta <oscarlagatta@Oscars-MacBook-Pro.local>
2019-09-02 23:28:23 +0100

    http requests with dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 // BREAK THE WIDGET INTO TWO SEPARATE CLASSES
 class App extends StatefulWidget {
@@ -17,6 +18,18 @@ class AppState extends State<App> {
   // that represent the data that change over time
   int counter = 0;
 
+// http://jsonplaceholder.typicode.com/photos/1000
+  void fetchImage() {        
+    
+    counter += 1;
+    get('http://jsonplaceholder.typicode.com/photos/$counter');
+    
+    setState(() {
+      counter += 1; 
+    });
+    
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -24,11 +37,8 @@ class AppState extends State<App> {
         floatingActionButton: FloatingActionButton( 
           
           child: Icon(Icons.add),
-          onPressed: () {           
-            setState(() {
-              counter += 1; 
-            });
-            } ,),
+          onPressed: fetchImage,
+          ),
         appBar: AppBar( 
           title: Text('Let\'s see some Images'),),
         ),
